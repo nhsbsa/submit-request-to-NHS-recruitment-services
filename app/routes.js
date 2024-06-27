9665c97e4dd5715cdc96bd4b8e34417c3621b9f4
katie.twinn@nhsbsa.nhs.uk <katie.twinn@nhsbsa.nhs.uk>
2024-06-27 16:28:48 +0100

    removing visa details from what other change

--- app/routes.js
+++ app/routes.js
@@ -2,6 +2,18 @@
 const express = require('express');
 const router = express.Router();
 
+// log stuff - thanks Craig Abbott!
+
+router.use((req, res, next) => {
+    const log = {
+        method: req.method,
+        url: req.originalUrl, //URL of page
+        data: req.session.data //all data held
+    }
+    console.log(JSON.stringify(log, null, 2)) // show all data as a dump in terminal
+    next() // continue to next action
+})
+
 // ****************************************
 // Route File Versions
 // ****************************************
